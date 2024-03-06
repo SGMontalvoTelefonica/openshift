96971c7d1231e27585f72d6906bb0e2fd02a5774
SGarciaMontalvo <sergio.garciamontalvo@telefonica.com>
2024-03-06 14:01:21 +0100

    Con dos páginas

--- public/index.php
+++ public/index.php
@@ -1,28 +1,7 @@
-<?php
+<html></html>
 
-    echo '<h1>Hola Sergio</H1>';
+    <h1>Hola Sergio</H1>
 
-    //Nos conectamos a la bbdd
-    try {
+    <a href="datos.php"> Ver los datos</a>
 
-        echo '<p>Conectando a base de datos</p>';
-
-        //Asignamos la conexión
-        $db = new mysqli($_ENV['MYSQL_DATABASE'], $_ENV['MYSQL_USER'], $_ENV['MYSQL_PASSWORD'], $dataBase);
-
-        //Si se conecta a la base de datos, retorna la conexión    
-        return $db;
-
-        echo '<p>¡Éxito!/p>';
-
-    } 
-    catch (\Throwable $th) {
-
-        echo("En este momento no se pudo conectar a la base de datos. Disculpe las molestias.\n
-            Código de error: " . $th->getCode() . " --> " . $th->getMessage()
-        );
-
-        exit;
-    }
-
-?>
+</html>
