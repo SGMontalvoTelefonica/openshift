7b4c4f5266dc136b0c3ef130dcbd29d0eb7b628d
SGMontalvoTelefonica <135319887+SGMontalvoTelefonica@users.noreply.github.com>
2024-04-15 14:07:18 +0200

    Create ip.php

---
+++ public/ip.php
@@ -0,0 +1,16 @@
+<html>
+
+    <h1>Viendo los datos</H1>
+
+    <p>Edición 03/04/2024 - B</p>
+
+    <a href="index.php"> Volver al index</a>
+
+</html>
+
+<?php
+
+  echo($_SERVER['REMOTE_ADDR']);
+  echo($_SERVER['HTTP_X_FORWARDED_FOR']);
+
+?>
